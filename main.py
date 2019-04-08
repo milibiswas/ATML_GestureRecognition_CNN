c4a2d517706e76ff5671cd7dd83e4d2a3a3b687c
BiswasM <mili.biswas@unifr.ch>
2019-04-08 20:19:37 +0200

    added different folder and file

---
+++ main.py
@@ -0,0 +1,5 @@
+# This is the main python script that will run the othe programs
+# Written by Mili Biswas (UNIFR)
+# Project  : American Sign Language Detection
+# Subject : Advanced Topics in Machine Learning (UniBE)
+
